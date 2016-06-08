bebe2dc5601bdcff0d95db11619cf34d3a8f38df
Mathias Jakobsen <mathias@jbcoding.com>
2016-06-09 00:34:55 +0200

    Added new Vector type with useful methods

---
+++ src/Vector2D.java
@@ -0,0 +1,48 @@
+/**
+ * Created by mathias on 09/06/16
+ */
+public class Vector2D {
+    public float x, y;
+
+    public Vector2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2D(Vector2D vec) {
+        this(vec.x, vec.y);
+    }
+
+    public Vector2D scale(float factor) {
+        return new Vector2D(x * factor, y * factor);
+    }
+
+    public Vector2D translate(Vector2D vec) {
+        return new Vector2D(x + vec.x, y + vec.y);
+    }
+
+    public Vector2D transform(double angle) {
+        return transform(new Vector2D(0,0), angle);
+    }
+
+    public Vector2D transform(Vector2D point, double angle) {
+        Vector2D vec = new Vector2D(x - point.x, y - point.y); //New vector at origo
+        double thisAngle = Math.atan2(vec.y, vec.x) + angle; //Angle of new vector
+        double vectorLength = vec.length();
+
+        Vector2D newVec = new Vector2D(
+            (float) ((Math.cos(thisAngle) * vectorLength) + point.x),
+            (float) ((Math.sin(thisAngle) * vectorLength) + point.y)
+        );
+
+        return newVec;
+    }
+
+    public double length() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+
+    public double distance(Vector2D vec) {
+        return Math.sqrt(Math.pow(x - vec.x, 2) + Math.pow(y - vec.y, 2));
+    }
+}
